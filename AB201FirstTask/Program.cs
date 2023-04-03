252aea28015e0aad19d28d6427bedf3bf059b7a8
zahid2000 <zahidmamedov07@gmail.com>
2023-04-03 20:43:06 +0400

    first Lesson

---
+++ AB201FirstTask/Program.cs
@@ -0,0 +1,131 @@
+﻿
+#region Variables
+//string name = "Hikmət";
+//var a = 5;
+
+//dynamic b;
+//b = 5;
+//Console.WriteLine(b);
+//b = true;
+//Console.WriteLine(b);
+
+
+#endregion
+
+#region Conditions
+//int age = 17;
+//if (age>16)
+//{
+//    Console.WriteLine("Age >16");
+//}
+//else if (age == 16)
+//{
+//    Console.WriteLine("Age == 16");
+//}
+//else
+//{
+//    Console.WriteLine("Age < 16");
+//}
+
+
+int day = 4;
+
+switch (day)
+{
+
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Weekend");
+        break;
+    default:
+        Console.WriteLine("Wrong number");
+        break;
+}
+
+//int month= 3;
+//string name = "Hikmet";
+//switch (name)
+//{
+//    case "Hikmet" when name.Length > 5:
+//        switch (name)
+//        {
+//            case "Hikmet":
+//                Console.WriteLine("Test");
+//                break;
+//            default:
+//                break;
+//        }
+//        break;
+//    case "Hikmet" when name.Length==5:
+//        Console.WriteLine("Ok");
+//        break;
+
+//}
+
+#endregion
+
+#region Loops
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//infinite loop->for
+//for (; ; )
+//{
+//    Console.WriteLine(5);
+//}
+//for (int i=0; true;i++)
+//{
+//    Console.WriteLine("Hello");
+//}
+//int i = 0;
+//while (i < 10)
+//{
+
+//    i++;
+//    if (i != 2)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//int num = 5;
+//do
+//{
+//   Console.WriteLine(num);
+
+//} while (num>5);
+
+
+
+//int[] arr = { 1, 2, 3, 12, 14, 15 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//foreach (int item in arr)
+//{
+//    if (item % 2 == 0)
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+#endregion
