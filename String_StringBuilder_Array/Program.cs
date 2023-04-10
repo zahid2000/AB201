1585263d8f91c2ffd98338d50113065601083a6f
zahid2000 <zahidmamedov07@gmail.com>
2023-04-10 20:05:26 +0400

    strings method

--- String_StringBuilder_Array/Program.cs
+++ String_StringBuilder_Array/Program.cs
@@ -6,10 +6,10 @@
 
 //string name3 = "Hikmet";
 //name = name3;
-using System.Linq.Expressions;
-using System.Text;
+//using System.Linq.Expressions;
+//using System.Text;
 
-string name = "Eli";
+//string name = "Eli";
 
 
 //string name4 = "Esli";
@@ -44,14 +44,97 @@ string name = "Eli";
 //    return result.ToString();
 //}
 
-string test = "       Eli     ";
+//string test = "       Eli     ";
 
 
-Console.WriteLine(test.Trim());
+//Console.WriteLine(test.Trim());
+//Console.WriteLine(test.Replace('i', 'a'));
+//bool StartWith(string str,char checkValue)
+//{
+//    return str[0] == checkValue;
+//}
+//int[] arr = { 1, 2, 3, 4, 5 };
+//Console.WriteLine("Nece eded daxil edeceksen?Sayi daxil et:");
+//int count = int.Parse(Console.ReadLine());
+//Student[] students = new Student[count];
+//Console.WriteLine("Hello");
+//Student st= new Student()
+//immutable
 
-Console.WriteLine(test.Replace('i', 'a'));
-bool StartWith(string str,char checkValue)
+//string name1 = "Eli";
+//string name2 = name1;
+//name1 = "Eli2";
+//string name3 = "Eli";
+//string name5 = "Eli";
+//String name4 = new String("Eli");
+////Console.WriteLine(name3==name4);
+////Console.WriteLine(ReferenceEquals(name4, name3));
+//Student stu = new Student { Name = "Muslum" };
+//Student stu1 = new Student { Name = "Muslum" };
+//Console.WriteLine(stu.Name==stu1.Name);
+//class Student 
+//{
+//    public Student()
+//    {
+
+//    }
+//    public string Name { get; set; }
+//    string GetName()
+//    {
+//        return Name;
+//    }
+//}
+
+//class Person
+//{
+//}
+string name = "1Hello Worldo salam dunya Ab201";
+//Console.WriteLine(name);
+//Console.WriteLine(name.EndsWith("Hello"));
+//Console.WriteLine(name.Contains("o"));
+
+//Console.WriteLine(name.IndexOf('o', name.IndexOf('o', name.IndexOf('o') + 1)+1));
+//Console.WriteLine(name.LastIndexOf('o'));
+
+//Console.WriteLine(name.Replace("Hello","Salam"));
+//Console.WriteLine(name.Remove(name.IndexOf('o'),2));
+//Console.WriteLine(name.Substring(,4));
+
+//Console.WriteLine(name.Length);
+//Console.WriteLine(name.Trim().Length);
+string[] arr = name.Split(' ');
+
+foreach (string s in arr)
 {
-    return str[0] == checkValue;
+    Console.WriteLine(s);
 }
 
+
+//string[] nums = { "salam","Hello","World"};
+//Console.WriteLine(string.Join('-',nums,1,2));
+
+//Console.WriteLine(string.Compare("abc","c"));
+//string name10 = " ";
+//if (string.IsNullOrEmpty(name10.Trim()))
+//{
+//    Console.WriteLine("IsnullOr Empty");
+
+//}
+//else
+//{
+//    Console.WriteLine(true);
+//}
+//Console.WriteLine(name.ToLower());
+//Console.WriteLine(name.ToUpper());
+
+
+//string test = "Hakim";
+//string check=Console.ReadLine();
+//if (check.ToLower() == test.ToLower())
+//{
+//    Console.WriteLine("var");
+//}
+//else
+//{
+//    Console.WriteLine("Yoxdur");
+//}
